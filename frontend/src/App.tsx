ac1bf1688a421bfc7281f45891a713818fc241f1
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-03-20 16:57:53 +0530

    refactor: Frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -7,8 +6,8 @@ import About from './pages/About';
 function App() {
   return (
       <Routes>
-        <Route path='/' element= {Home}/>
-        <Route path='/about' element= {About}/>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/about' element= {<About/>}/>
       </Routes>
   );
 }
