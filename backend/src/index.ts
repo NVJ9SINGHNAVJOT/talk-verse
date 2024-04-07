0dd0b486e1c2dac34dbacb19f933d10518889f96
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-04-08 00:56:30 +0530

    refactor(development): dotenv import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import app from '@/app';
 import { setupSocketIO } from '@/socket/index';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT: number = parseInt(process.env.PORT as string) || 4000;
 
