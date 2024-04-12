53d61bf665bdb661962c1c457b2a05360f7784ef
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-04-12 17:24:05 +0530

    refactor(development): schema separate in postgresql in server1

--- backend/server1/src/db/postgresql/schema.ts
+++ backend/server1/src/db/postgresql/schema.ts
@@ -1,46 +1,5 @@
-import { relations } from "drizzle-orm";
-import { serial, text, timestamp, integer, pgTable } from "drizzle-orm/pg-core";
+import * as user from "@/db/postgresql/schema/user";
+import * as post from "@/db/postgresql/schema/post";
+import * as comment from "@/db/postgresql/schema/comment";
 
-export const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull(),
-    number: integer("number"),
-    imageUrl: text("image_url"),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
-
-export const posts = pgTable("posts", {
-    id: serial("id").primaryKey(),
-    title: text("title").notNull(),
-    content: text("content").notNull(),
-    userId: integer("user_id")
-        .notNull()
-        .references(() => users.id),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
-
-export const comments = pgTable("comments", {
-    id: serial("id").primaryKey(),
-    postId: integer("post_id").references(() => posts.id),
-    userId: integer("user_id").references(() => users.id),
-    text: text("text").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
-
-export const postsRelations = relations(posts, ({ one, many }) => ({
-    user: one(users, { fields: [posts.userId], references: [users.id] }),
-    comments: many(comments),
-}));
-
-export const usersRelations = relations(users, ({ many }) => ({
-    posts: many(posts),
-}));
-
-export const commentsRelations = relations(comments, ({ one }) => ({
-    post: one(posts, { fields: [comments.postId], references: [posts.id] }),
-    user: one(users, { fields: [comments.userId], references: [users.id] }),
-}));
+export const schema = { user, post, comment };
---
+++ backend/server1/src/db/postgresql/schema/post.ts
@@ -0,0 +1,21 @@
+import { relations } from "drizzle-orm";
+import { pgTable, integer, serial, timestamp, text } from "drizzle-orm/pg-core";
+import { comment } from "@/db/postgresql/schema/comment";
+import { user } from "@/db/postgresql/schema/user";
+
+export const post = pgTable("post", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    content: text("content").notNull(),
+    userId: integer("user_id")
+        .notNull()
+        .references(() => user.id),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+
+export const postsRelations = relations(post, ({ one, many }) => ({
+    use: one(user, { fields: [post.userId], references: [user.id] }),
+    comment: many(comment),
+}));
