4ad044a02ddfa4b81fef21c8880c0b48ca81116a
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-04-14 19:09:30 +0530

    feat(development): shadcn-ui
    shadcn-ui settings create and index.css refactor
    
    index.css and tailwind.config.js have settings for shadcn-ui

---
+++ frontend/src/components/login/loginrightside/LoginRightSide.tsx
@@ -0,0 +1,30 @@
+import "@/components/login/loginrightside/LoginRightSide.css"
+
+const LoginRightSide = () => {
+    return (
+        <div className=" w-1/2 h-full]">
+
+            <div className="loginSidePage" >
+
+                <strong className="strongForLoginPage">
+                    <div className=" text-center text-4xl text-white pl-8 pr-8">
+                        Welcome to <span className=" blinkingHeading  text-richblack-400">TalkVerse! </span>
+                        Connect, share, and engage in conversations that matter. Log in to your world of words and wisdom.
+                    </div>
+                </strong>
+                <div id="container-stars">
+                    <div id="stars"></div>
+                </div>
+
+                <div id="glow">
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginRightSide
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LogIn from "@/components/auth/LogIn";
 import SignUp from "@/components/auth/SignUp";
+import LoginRightSide from "@/components/login/loginrightside/LoginRightSide";
 
 
 const Login = () => {
@@ -22,28 +23,7 @@ const Login = () => {
 
 
       {/* right part */}
-      <div className=" w-1/2 h-full]">
-
-        <div className="loginSidePage" >
-
-          <strong className="strongForLoginPage">
-            <div className=" text-center text-4xl text-white pl-8 pr-8">
-              Welcome to <span className=" blinkingHeading  text-richblack-400">TalkVerse! </span>
-              Connect, share, and engage in conversations that matter. Log in to your world of words and wisdom.
-            </div>
-          </strong>
-          <div id="container-stars">
-            <div id="stars"></div>
-          </div>
-
-          <div id="glow">
-            <div className="circle"></div>
-            <div className="circle"></div>
-          </div>
-
-        </div>
-
-      </div>
+      <LoginRightSide />
 
 
     </div>
