8f5bfc52ec49002b811ef5cd608e7072f2f505b4
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-05 14:13:56 +0530

    build(development): npm command
    postgres migrate and trigger command removed from scripts as they are included in index.ts file

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,18 +1,37 @@
+import { useAppSelector } from "@/redux/store";
+import { RxAvatar } from "react-icons/rx";
 import { Outlet } from "react-router-dom";
 
 const Blog = () => {
+  const user = useAppSelector((state) => state.user.user);
+
   return (
-    <div className="w-full flex h-[calc(100vh-4rem)] min-w-minContent">
+    <div className="w-full flex h-[calc(100vh-4rem)] min-w-minContent bg-neutral-950">
       {/* user profile and category section  */}
-      <section className=" w-48 lm:w-56 bg-black"></section>
+      <section className=" w-40 lg:w-48  bg-white">
+        {/* user profile details */}
+        <div className=" flex flex-col mt-8 text-snow-200 font-be-veitnam-pro items-center gap-y-6">
+          {user?.imageUrl ? (
+            <img
+              src={user?.imageUrl}
+              alt="Loading..."
+              className=" size-24 rounded-full aspect-auto"
+            />
+          ) : (
+            <RxAvatar className=" w-full size-24 rounded-full aspect-auto fill-slate-500" />
+          )}
+          {user && <p>{user?.firstName + user?.lastName}</p>}
+          {user && <p>{user.userName}</p>}
+        </div>
+      </section>
       {/* posts section */}
-      <section className=" w-32 bg-yellow-200">
+      <section className=" w-32 flex-1 ">
         {/* story section */}
         <section></section>
         <Outlet />
       </section>
       {/* create post and friend suggestion */}
-      <section className="lm:w-56 hidden lm:block  bg-red-800"></section>
+      <section className="hidden lm:block lm:w-40 lg:w-48"></section>
     </div>
   );
 };
