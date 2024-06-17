c1bf7305e197d9a404adc25d7cf95e17a346e038
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-17 18:39:47 +0530

    fix(development): click outside
    side menu click outside component separated

--- frontend/src/components/common/MainNavbar.tsx
+++ frontend/src/components/common/MainNavbar.tsx
@@ -1,23 +1,20 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import mainLogo from "@/assets/images/mainLogo.png";
 import SignInButton from "@/lib/buttons/signinbutton/SignInButton";
 import { useAppSelector } from "@/redux/store";
 import UserMenu from "@/components/common/UserMenu";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useRef, useState } from "react";
-import useOnClickOutside from "@/hooks/useOnClickOutside";
+import SideMenu from "@/components/common/SideMenu";
 
 const MainNavbar = () => {
-  const location = useLocation();
   const navigate = useNavigate();
   const user = useAppSelector((state) => state.user.user);
   const authUser = useAppSelector((state) => state.auth.authUser);
   const myPrivateKey = useAppSelector((state) => state.messages.myPrivateKey);
   const [menu, setMenu] = useState<boolean>(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const menuRefExclude = useRef<HTMLDivElement>(null);
 
-  useOnClickOutside(menuRef, () => setMenu(false), menuRefExclude);
+  const menuRefExclude = useRef<HTMLDivElement>(null);
 
   const toogleMenu = () => {
     setMenu((prev) => !prev);
@@ -135,33 +132,15 @@ const MainNavbar = () => {
 
       {/* menu toggle for small screen */}
       {menu && (
-        <div
-          ref={menuRef}
-          className={`${
-            location.pathname.includes("about") ? "text-black" : "text-white"
-          }  flex flex-col z-[1000] md:hidden absolute top-[4rem] right-0 backdrop-blur-md
-            justify-start h-[calc(100vh-4rem)] items-center w-4/12 gap-y-4`}
-        >
-          <div
-            className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed] 
-              mt-4 "
-            onClick={homeHandler}
-          >
-            Home
-          </div>
-          <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={aboutHandler}>
-            About
-          </div>
-          <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={contactHandler}>
-            Contact
-          </div>
-          <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={talkHandler}>
-            Talk
-          </div>
-          <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={blogHandler}>
-            Blog
-          </div>
-        </div>
+        <SideMenu
+          menuRefExclude={menuRefExclude}
+          setMenu={setMenu}
+          homeHandler={homeHandler}
+          aboutHandler={aboutHandler}
+          contactHandler={contactHandler}
+          talkHandler={talkHandler}
+          blogHandler={blogHandler}
+        />
       )}
     </div>
   );
---
+++ frontend/src/components/common/SideMenu.tsx
@@ -0,0 +1,54 @@
+import useOnClickOutside from "@/hooks/useOnClickOutside";
+import { useRef } from "react";
+import { useLocation } from "react-router-dom";
+
+type SideMenuProps = {
+  menuRefExclude: React.RefObject<HTMLDivElement>;
+  setMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  homeHandler: () => void;
+  aboutHandler: () => void;
+  contactHandler: () => void;
+  talkHandler: () => void;
+  blogHandler: () => void;
+};
+
+const SideMenu = (props: SideMenuProps) => {
+  const location = useLocation();
+  const menuRef = useRef<HTMLDivElement>(null);
+  useOnClickOutside(menuRef, () => props.setMenu(false), props.menuRefExclude);
+
+  return (
+    <div
+      ref={menuRef}
+      className={`${
+        location.pathname.includes("about") ? "text-black" : "text-white"
+      }  flex flex-col z-[1000] md:hidden absolute top-[4rem] right-0 backdrop-blur-md
+    justify-start h-[calc(100vh-4rem)] items-center w-4/12 gap-y-4`}
+    >
+      <div
+        className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed] 
+      mt-4 "
+        onClick={props.homeHandler}
+      >
+        Home
+      </div>
+      <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={props.aboutHandler}>
+        About
+      </div>
+      <div
+        className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+        onClick={props.contactHandler}
+      >
+        Contact
+      </div>
+      <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={props.talkHandler}>
+        Talk
+      </div>
+      <div className="cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]" onClick={props.blogHandler}>
+        Blog
+      </div>
+    </div>
+  );
+};
+
+export default SideMenu;
