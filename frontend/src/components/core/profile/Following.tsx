acfed4cbbe848b6bed4b5687cfb6852a9fb23413
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-18 01:42:55 +0530

    feat(development): other user
    other user component in progress

--- frontend/src/components/core/profile/Following.tsx
+++ frontend/src/components/core/profile/Following.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { useScrollTriggerVertical } from "@/hooks/useScrollTrigger";
+import OtherUser from "@/components/core/profile/otheruser/OtherUser";
+import { userFollowingApi } from "@/services/operations/profileApi";
+import { UserSuggestion } from "@/types/apis/notificationApiRs";
+import { useEffect, useRef, useState } from "react";
+import { toast } from "react-toastify";
 
 const Following = () => {
-  return <div>Following</div>;
+  const [stop, setStop] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [trigger, setTrigger] = useState<boolean>(true);
+  const followingContainer = useRef<HTMLDivElement>(null);
+  const [following, setFollowing] = useState<(UserSuggestion & { createdAt: string })[]>([]);
+
+  useScrollTriggerVertical(followingContainer, "down", setTrigger, stop, undefined, loading);
+
+  const removeOtherUser = (otherUserId: number) => {
+    setFollowing((prev) => prev.filter((otherUser) => otherUser.id !== otherUserId));
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) {
+        return;
+      }
+      setLoading(true);
+      let lastCreatedAt;
+      if (following.length === 0) {
+        lastCreatedAt = new Date().toISOString();
+      } else {
+        lastCreatedAt = following[following.length - 1].createdAt;
+      }
+
+      const response = await userFollowingApi(lastCreatedAt);
+      if (response) {
+        if (response.following) {
+          const withNewPosts = [...following, ...response.following];
+          if (response.following.length < 20) {
+            setStop(true);
+          }
+          setFollowing(withNewPosts);
+        } else {
+          setStop(true);
+        }
+      } else {
+        toast.error("Error while getting following users");
+      }
+      setLoading(false);
+    };
+    getUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [trigger]);
+
+  return (
+    <div
+      ref={followingContainer}
+      className="w-full h-full flex flex-wrap justify-center p-7 lg:p-12 gap-4 lg:gap-7 xl:gap-10
+     overflow-y-auto bg-[linear-gradient(135deg,_#fdfcfb_0%,_#e2d1c3_100%)]"
+    >
+      {following.length > 0 && following.map((otherUser, index) => <OtherUser key={index} />)}
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+      <OtherUser />
+    </div>
+  );
 };
 
 export default Following;
---
+++ frontend/src/components/core/profile/otheruser/OtherUser.tsx
@@ -0,0 +1,13 @@
+const OtherUser = () => {
+  return (
+    <div
+      className=" relative w-44 aspect-square  hover:bg-orange-200 bg-[linear-gradient(to_right,_#16222A_0%,_#3A6073_51%,_#16222A_100%)]
+      [transition:0.5s] [background-size:200%_auto] [box-shadow:0_0_20px_#eee] hover:bg-[right_center] rounded-lg 
+       hover:scale-125 transition-all duration-100 ease-in-out hover:z-50 "
+    >
+      
+    </div>
+  );
+};
+
+export default OtherUser;
