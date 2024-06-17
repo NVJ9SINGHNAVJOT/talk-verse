2194e96f578fa95e2c25c956360c447b426be810
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-17 19:11:29 +0530

    fix(development): friend requests
    separate component made for friend requests

--- frontend/src/components/core/talk/chatItems/UserChatBar.tsx
+++ frontend/src/components/core/talk/chatItems/UserChatBar.tsx
@@ -6,18 +6,12 @@ import CreateGroup from "@/components/core/talk/chatItems/CreateGroupModal";
 import { useSocketContext } from "@/context/SocketContext";
 import userChatBarEvents from "@/socket/events/userChatBarEvents";
 import { useRef, useState } from "react";
-import useOnClickOutside from "@/hooks/useOnClickOutside";
 import { useAppSelector } from "@/redux/store";
-import { acceptRequestApi, deleteRequestApi } from "@/services/operations/notificationApi";
-import { RxAvatar } from "react-icons/rx";
-import { CiCirclePlus } from "react-icons/ci";
-import { addChatBarData, addFriend, ChatBarData, deleteUserRequest, Friend } from "@/redux/slices/chatSlice";
-import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
 import GroupBarItem from "@/components/core/talk/chatItems/GroupBarItem";
-import { addNewUnseen, addPublicKey, PublicKey } from "@/redux/slices/messagesSlice";
 import { SoAddedInGroup } from "@/types/socket/eventTypes";
 import FriendBarItem from "@/components/core/talk/chatItems/FriendBarItem";
+import FriendRequests from "@/components/core/talk/chatItems/FriendRequests";
+import { Friend } from "@/redux/slices/chatSlice";
 
 const UserChatBar = () => {
   const [inChat, setInChat] = useState<string>("");
@@ -25,50 +19,11 @@ const UserChatBar = () => {
   const [isCreateGroupOpen, setIsCreateGroupOpen] = useState(false);
   const [seeNotif, setSeeNotif] = useState(false);
   const { socket } = useSocketContext();
-  const seeNotifRef = useRef<HTMLDivElement>(null);
   const excSeeNotifRef = useRef<HTMLDivElement>(null);
   const userRequests = useAppSelector((state) => state.chat.userRequests);
   const chatBarData = useAppSelector((state) => state.chat.chatBarData);
-  useOnClickOutside(seeNotifRef, () => setSeeNotif(false), excSeeNotifRef);
-  const dispatch = useDispatch();
 
   userChatBarEvents(socket);
-
-  const acceptReq = async (userId: string) => {
-    dispatch(deleteUserRequest(userId));
-    const response = await acceptRequestApi(userId);
-    if (response && response.success === true) {
-      const newData: Friend & ChatBarData = {
-        _id: response.newFriend._id,
-        firstName: response.newFriend.firstName,
-        lastName: response.newFriend.lastName,
-        imageUrl: response.newFriend.imageUrl,
-        chatId: response.newChatId,
-      };
-      dispatch(
-        addPublicKey({
-          userId: response.newFriend._id,
-          publicKey: response.newFriendPublicKey,
-        } as PublicKey)
-      );
-      dispatch(addFriend(newData));
-      dispatch(addNewUnseen(response.newChatId));
-      dispatch(addChatBarData(newData));
-      toast.success("New friend added");
-    } else {
-      toast.error("Error while adding friend");
-    }
-  };
-
-  const deleteReq = async (userId: string) => {
-    dispatch(deleteUserRequest(userId));
-    const response = await deleteRequestApi(userId);
-    if (response) {
-      toast.info("Request deleted");
-    } else {
-      toast.error("Error while deleting request");
-    }
-  };
 
   const toggleSearchModal = () => {
     setIsSearchOpen(!isSearchOpen);
@@ -94,40 +49,7 @@ const UserChatBar = () => {
               onClick={() => setSeeNotif((prev) => !prev)}
             />
           </div>
-          {seeNotif && (
-            <div
-              ref={seeNotifRef}
-              className=" absolute z-[500] -top-3 -right-[18rem] rounded-xl gap-y-3 text-white flex flex-col px-5 py-2 bg-black"
-            >
-              {userRequests?.length === 0 ? (
-                <div className=" bg-black px-3 py-1 text-white text-center w-[12rem]">No Requests</div>
-              ) : (
-                userRequests?.map((user, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className=" flex w-fit items-center gap-x-3 bg-black hover:bg-gray-800 px-3 py-1 rounded-lg"
-                    >
-                      {user.imageUrl ? (
-                        <img src={user.imageUrl} className=" rounded-full w-10 h-10 aspect-auto" alt="Loading..." />
-                      ) : (
-                        <RxAvatar className="w-10 h-10 aspect-auto" />
-                      )}
-                      <div className=" truncate">{user.userName}</div>
-                      <CiCirclePlus
-                        onClick={() => acceptReq(user._id)}
-                        className=" text-white w-8 h-8 aspect-auto cursor-pointer hover:bg-white hover:text-black rounded-full"
-                      />
-                      <CiCirclePlus
-                        onClick={() => deleteReq(user._id)}
-                        className=" text-white w-8 h-8 aspect-auto cursor-pointer rotate-45 hover:bg-white hover:text-black rounded-full"
-                      />
-                    </div>
-                  );
-                })
-              )}
-            </div>
-          )}
+          {seeNotif && <FriendRequests setSeeNotif={setSeeNotif} excSeeNotifRef={excSeeNotifRef} />}
         </div>
       </div>
 
