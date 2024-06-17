e7131b73599c57ab8614f16324ab1d6727594a8c
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-17 18:29:05 +0530

    fix(useClickOutside): click out side
    click out side hook was working continuously, fixed buy making  separate component

---
+++ frontend/src/components/common/DropDownUserMenu.tsx
@@ -0,0 +1,54 @@
+import useOnClickOutside from "@/hooks/useOnClickOutside";
+import LogOutModal from "@/components/auth/LogOutModal";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+type DropDownUserMenuProps = {
+  userMenuRef: React.RefObject<HTMLDivElement>;
+  setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DropDownUserMenu = (props: DropDownUserMenuProps) => {
+  const navigate = useNavigate();
+  const [togLogO, setTogLogO] = useState<boolean>(false);
+
+  useOnClickOutside(props.userMenuRef, () => props.setMenuOpen(false));
+
+  return (
+    <div
+      className="gap-1 absolute top-16 -left-[6rem] flex flex-col justify-start 
+    z-[500] text-white w-36 bg-black py-2 rounded-sm"
+    >
+      <div
+        onClick={() => {
+          props.setMenuOpen(false);
+          navigate("/profile");
+        }}
+        className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
+      >
+        Profile
+      </div>
+      <div
+        onClick={() => {
+          props.setMenuOpen(false);
+          navigate("/profile/settings");
+        }}
+        className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
+      >
+        Settings
+      </div>
+      <div
+        onClick={() => {
+          props.setMenuOpen(false);
+          setTogLogO(true);
+        }}
+        className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
+      >
+        Log Out
+      </div>
+      {togLogO && <LogOutModal setTogLogO={setTogLogO} setMenuOpen={props.setMenuOpen} />}
+    </div>
+  );
+};
+
+export default DropDownUserMenu;
--- frontend/src/components/common/UserMenu.tsx
+++ frontend/src/components/common/UserMenu.tsx
@@ -1,32 +1,20 @@
-import useOnClickOutside from "@/hooks/useOnClickOutside";
 import { useAppSelector } from "@/redux/store";
 import { useRef, useState } from "react";
-import LogOutModal from "@/components/auth/LogOutModal";
-import { useNavigate } from "react-router-dom";
+import DropDownUserMenu from "@/components/common/DropDownUserMenu";
 
 const UserMenu = () => {
   const user = useAppSelector((state) => state.user.user);
-  const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
-  const [togLogO, setTogLogO] = useState<boolean>(false);
   const userMenuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
   };
 
-  useOnClickOutside(userMenuRef, () => setMenuOpen(false));
-
   if (!user) {
     return <div>Error</div>;
   }
 
-  let first;
-  let second;
-  if (!user.imageUrl) {
-    first = user.firstName.charAt(0);
-    second = user.lastName.charAt(0);
-  }
   return (
     <div ref={userMenuRef} className=" relative flex justify-evenly items-center">
       {user.imageUrl ? (
@@ -42,44 +30,12 @@ const UserMenu = () => {
           className=" peer bg-richblue-500 text-white w-10 h-10 rounded flex 
                     cursor-pointer justify-center items-center gap-1 hover:bg-transparent"
         >
-          <span className=" uppercase text-white text-xl">{first}</span>
-          <span className=" uppercase text-white text-xl">{second}</span>
+          <span className=" uppercase text-white text-xl">{user.firstName.charAt(0)}</span>
+          <span className=" uppercase text-white text-xl">{user.lastName.charAt(0)}</span>
         </div>
       )}
       {/* drop down menu */}
-      <div
-        className={`${menuOpen ? "block" : "hidden"} gap-1 absolute top-16 -left-[6rem] flex flex-col justify-start 
-          z-[500] text-white w-36 bg-black py-2 rounded-sm`}
-      >
-        <div
-          onClick={() => {
-            setMenuOpen(false);
-            navigate("/profile");
-          }}
-          className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
-        >
-          Profile
-        </div>
-        <div
-          onClick={() => {
-            setMenuOpen(false);
-            navigate("/profile/settings");
-          }}
-          className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
-        >
-          Settings
-        </div>
-        <div
-          onClick={() => {
-            setMenuOpen(false);
-            setTogLogO(true);
-          }}
-          className=" cursor-pointer bg-black hover:bg-grayblack py-1 px-6"
-        >
-          Log Out
-        </div>
-      </div>
-      {togLogO && <LogOutModal setTogLogO={setTogLogO} setMenuOpen={setMenuOpen} />}
+      {menuOpen && <DropDownUserMenu userMenuRef={userMenuRef} setMenuOpen={setMenuOpen} />}
     </div>
   );
 };
