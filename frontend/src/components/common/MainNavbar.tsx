40a8cfc38a1bc37d2c078a08708032880bab4484
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-19 19:32:06 +0530

    refactor(development): navigate
    talk page navigate from menu bar changed

--- frontend/src/components/common/MainNavbar.tsx
+++ frontend/src/components/common/MainNavbar.tsx
@@ -45,14 +45,11 @@ const MainNavbar = () => {
     if (menu) {
       setMenu(false);
     }
-    if (authUser) {
-      if (myPrivateKey !== undefined) {
-        navigate("/talk");
-      } else {
-        navigate("/checkKey");
-      }
+
+    if (myPrivateKey === undefined && authUser) {
+      navigate("/checkKey");
     } else {
-      navigate("/login");
+      navigate("/talk");
     }
   };
 
@@ -64,50 +61,43 @@ const MainNavbar = () => {
   };
 
   return (
-    <div
-      className="relative bg-[radial-gradient(circle_at_24.1%_68.8%,_rgb(50,_50,_50)_0%,_rgb(0,_0,_0)_99.4%)]
-        h-[4rem] flex justify-between items-center w-full"
-    >
+    <div className="relative flex h-[4rem] w-full items-center justify-between bg-[radial-gradient(circle_at_24.1%_68.8%,_rgb(50,_50,_50)_0%,_rgb(0,_0,_0)_99.4%)]">
       {/* main logo and name */}
-      <div className="flex items-center justify-evenly gap-5 ml-8">
+      <div className="ml-8 flex items-center justify-evenly gap-5">
         <img alt="Logo" src={mainLogo} className="h-14 cursor-pointer" onClick={homeHandler} />
-        <div
-          className="ct-mainLogoButton text-2xl font-semibold  text-richblack-25 "
-          role="button"
-          onClick={homeHandler}
-        >
+        <div className="ct-mainLogoButton text-2xl font-semibold text-richblack-25" role="button" onClick={homeHandler}>
           TalkVerse
         </div>
       </div>
 
       {/* navbar menu */}
-      <div className="hidden md:flex justify-evenly items-center md:gap-4 lm:gap-6 lg:gap-8">
+      <div className="hidden items-center justify-evenly md:flex md:gap-4 lm:gap-6 lg:gap-8">
         <div
-          className=" text-white  cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+          className="round cursor-pointer rounded-sm text-white hover:[text-shadow:0_0_5px_#59deed]"
           onClick={homeHandler}
         >
           Home
         </div>
         <div
-          className=" text-white  cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+          className="round cursor-pointer rounded-sm text-white hover:[text-shadow:0_0_5px_#59deed]"
           onClick={aboutHandler}
         >
           About
         </div>
         <div
-          className=" text-white  cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+          className="round cursor-pointer rounded-sm text-white hover:[text-shadow:0_0_5px_#59deed]"
           onClick={contactHandler}
         >
           Contact
         </div>
         <div
-          className=" text-white  cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+          className="round cursor-pointer rounded-sm text-white hover:[text-shadow:0_0_5px_#59deed]"
           onClick={talkHandler}
         >
           Talk
         </div>
         <div
-          className=" text-white  cursor-pointer round rounded-sm hover:[text-shadow:0_0_5px_#59deed]"
+          className="round cursor-pointer rounded-sm text-white hover:[text-shadow:0_0_5px_#59deed]"
           onClick={blogHandler}
         >
           Blog
@@ -116,19 +106,19 @@ const MainNavbar = () => {
 
       {/* sign in buttons or user logo */}
       {user ? (
-        <div className={`flex justify-evenly items-center md:gap-2 gap-x-2 mr-8 `}>
+        <div className={`mr-8 flex items-center justify-evenly gap-x-2 md:gap-2`}>
           <UserMenu />
           <div ref={menuRefExclude} onClick={toogleMenu} className="md:hidden">
-            <GiHamburgerMenu className="cursor-pointer w-6 h-8 ml-2 aspect-auto text-white rounded-sm" />
+            <GiHamburgerMenu className="ml-2 aspect-auto h-8 w-6 cursor-pointer rounded-sm text-white" />
           </div>
         </div>
       ) : (
-        <div className={`flex justify-evenly items-center gap-3 lg:gap-5 mr-8`}>
+        <div className={`mr-8 flex items-center justify-evenly gap-3 lg:gap-5`}>
           <SignInButton title={"Log In"} />
           <SignInButton title={"Sign Up"} />
 
-          <div ref={menuRefExclude} onClick={toogleMenu} className=" md:hidden">
-            <GiHamburgerMenu className="cursor-pointer w-6 h-8 ml-2 aspect-auto text-white rounded-sm" />
+          <div ref={menuRefExclude} onClick={toogleMenu} className="md:hidden">
+            <GiHamburgerMenu className="ml-2 aspect-auto h-8 w-6 cursor-pointer rounded-sm text-white" />
           </div>
         </div>
       )}
