3a6511c1319874b43d5a2a84f7775121932fa057
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-19 18:40:20 +0530

    fix(development): vite
    vite production server config added for backend

--- backend/src/config/corsOptions.ts
+++ backend/src/config/corsOptions.ts
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173 ", "http://localhost:4173"],
   credentials: true,
   methods: ["PUT", "PATCH", "POST", "GET", "DELETE"],
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     // vite config
