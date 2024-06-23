b7621ace1e070e4309e095abc188b853e37d252f
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-06-23 18:46:51 +0530

    fix(development): overflow
    navbar overflow x removed

--- frontend/src/components/common/MainNavbar.tsx
+++ frontend/src/components/common/MainNavbar.tsx
@@ -61,7 +61,7 @@ const MainNavbar = () => {
   };
 
   return (
-    <div className="relative mx-auto flex h-[4rem] w-full min-w-minContent max-w-maxContent items-center justify-between overflow-x-hidden bg-[radial-gradient(circle_at_24.1%_68.8%,_rgb(50,_50,_50)_0%,_rgb(0,_0,_0)_99.4%)]">
+    <div className="relative mx-auto flex h-[4rem] w-full min-w-minContent max-w-maxContent items-center justify-between bg-[radial-gradient(circle_at_24.1%_68.8%,_rgb(50,_50,_50)_0%,_rgb(0,_0,_0)_99.4%)]">
       {/* main logo and name */}
       <div className="ml-8 flex items-center justify-evenly gap-5">
         <img alt="Logo" src={mainLogo} className="h-14 cursor-pointer" onClick={homeHandler} />
