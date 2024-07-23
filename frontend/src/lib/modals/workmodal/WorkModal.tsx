3f67449207397acd021d79081a67b65a229435e4
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-07-23 23:21:32 +0530

    feat(development): private key modal added

--- frontend/src/lib/modals/workmodal/WorkModal.tsx
+++ frontend/src/lib/modals/workmodal/WorkModal.tsx
@@ -6,10 +6,10 @@ type WorkModalProps = {
 const WorkModal = (props: WorkModalProps) => {
   return (
     <div
-      className=" fixed inset-0 w-screen h-screen backdrop-blur bg-transparent z-[2000] 
-    flex flex-col gap-16 justify-center items-center"
+      className="fixed inset-0 w-screen h-screen backdrop-blur bg-transparent z-[2000] 
+      flex flex-col gap-16 justify-center items-center"
     >
-      <div className=" text-purple-500 font-bold text-4xl lm:text-6xl font-be-veitnam-pro">{props.title}</div>
+      <div className="text-purple-500 font-bold text-4xl lm:text-6xl font-be-veitnam-pro">{props.title}</div>
       <div className="workModal"></div>
     </div>
   );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,14 @@
 import LogIn from "@/components/auth/LogIn";
 import SignUp from "@/components/auth/SignUp";
+import SignUpSuccess from "@/components/auth/SignUpSuccess";
 import LoginRightSide from "@/lib/sections/loginrightside/LoginRightSide";
 import { setIsLogin } from "@/redux/slices/authSlice";
 import { useAppSelector } from "@/redux/store";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 const Login = () => {
+  const [success, setSuccess] = useState<boolean>(false);
   const isLogin = useAppSelector((state) => state.auth.isLogin);
   const dispatch = useDispatch();
   const toggleSignIn = () => {
@@ -19,11 +22,16 @@ const Login = () => {
     >
       {/* left part */}
       <div className="flex justify-center w-1/2 h-full]">
-        {isLogin ? <LogIn toggleSignIn={toggleSignIn} /> : <SignUp toggleSignIn={toggleSignIn} />}
+        {isLogin ? (
+          <LogIn toggleSignIn={toggleSignIn} />
+        ) : (
+          <SignUp toggleSignIn={toggleSignIn} setSuccess={setSuccess} />
+        )}
       </div>
 
       {/* right part */}
       <LoginRightSide />
+      {success && <SignUpSuccess setSuccess={setSuccess} />}
     </div>
   );
 };
