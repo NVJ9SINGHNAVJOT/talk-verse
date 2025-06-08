7ea3ae4f36d702c6a6a4e726d6dd0313f9221f7b
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2025-06-08 21:27:31 +0530

    refactor(login): className type fixed

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
       bg-[radial-gradient(circle_at_24.1%_68.8%,_rgb(50,_50,_50)_0%,_rgb(0,_0,_0)_99.4%)]"
     >
       {/* left part */}
-      <div className="flex justify-center w-1/2 h-full]">
+      <div className="flex justify-center w-1/2 h-full">
         {isLogin ? (
           <LogIn toggleSignIn={toggleSignIn} />
         ) : (
