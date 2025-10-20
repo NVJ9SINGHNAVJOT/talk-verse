652706ab8834d54d818fcc7b45883e7441b68f17
Navjot Singh <nvjsinghnavjot4411@gmail.com>
2025-10-20 20:59:25 +0530

    chore(scripts): updated

--- clone_files.sh
+++ clone_files.sh
@@ -27,22 +27,22 @@ current_time() {
 
 # Function to log information messages
 loginf() {
-    echo -e "$(current_time) \e[1;34m[INF]\e[0m $1"  # Bright blue for [INF]
+    printf "%s \033[1;34m[INF]\033[0m %s\n" "$(current_time)" "$1"
 }
 
 # Function to log error messages
 logerr() {
-    echo -e "$(current_time) \e[1;31m[ERR]\e[0m \e[0;31m$1\e[0m"  # Bright red for [ERR] and dull red for message
+    printf "%s \033[1;31m[ERR]\033[0m \033[0;31m%s\033[0m\n" "$(current_time)" "$1"
 }
 
 # Function to log warning messages
 war() {
-    echo -e "$(current_time) \e[1;33m[WAR]\e[0m $1"  # Yellow for [WAR]
+    printf "%s \033[1;33m[WAR]\033[0m %s\n" "$(current_time)" "$1"
 }
 
-# Function to log logsuccess messages
+# Function to log success messages
 logsuccess() {
-    echo -e "$(current_time) \e[1;32m[SUCCESS]\e[0m $1"  # Bright green for [SUCCESS]
+    printf "%s \033[1;32m[SUCCESS]\033[0m %s\n" "$(current_time)" "$1"
 }
 
 # Check if the destination directory exists
